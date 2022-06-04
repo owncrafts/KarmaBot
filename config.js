880b00597ad9428eee0ae9906b5d2887e3980b66
owncrafts <104671436+owncrafts@users.noreply.github.com>
2022-06-04 18:34:40 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,15 @@
+module.exports = {
+
+    token: 'OTgxMzgyMjk4MTg2NDI4NDM2.G0NrVj.5iEqj5c912yjVZ8o-J0aH06th5mfHh4R6NPiTI',      // Your Bot Token
+
+    prefix: 'zen',     // Your Prefix
+
+    devs: '980164839718678568',       // Owner's Discord ID
+
+    embedcolor: '#012d52', // EmbedColor
+
+    MongoURI: 'mongodb+srv://disco:disco@disco.kegte.mongodb.net/?retryWrites=true&w=majority',   // Database URL for storing Channel's Data.
+
+    AME_API: ''     // AME_API for Image Commands. Key can be found -> https://docs.api.amethyste.moe
+
+}
